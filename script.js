47b9f63964802058a064d29115a7dcfd4f42c479
CryptoCrane2601 <dejan.kurcubic@icloud.com>
2023-02-19 20:54:42 +0100

    switch active player

--- script.js
+++ script.js
@@ -3,9 +3,33 @@
 // Selecting elements
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1 ');
 const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 // Starting condition
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+let currentScore = 0;
+
+// Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  // 1. Generating random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  // 2. Display the dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+  // 3. Check if rolled  1:
+  if (dice !== 1) {
+    currentScore += dice;
+    current0El.textContent = currentScore; // Change later!
+    // Add dice to current score
+  } else {
+    // Switch to next player
+    currentScore = 0;
+  }
+});
