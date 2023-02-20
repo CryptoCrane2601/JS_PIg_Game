e30503b71690bc61810f483fa97a0b2cdafffd67
CryptoCrane2601 <dejan.kurcubic@icloud.com>
2023-02-20 23:47:21 +0100

    toogle class

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // Selecting elements
+const player0EL = document.querySelector('.player--0');
+const player1EL = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
@@ -34,7 +36,10 @@ btnRoll.addEventListener('click', function () {
     // Add dice to current score
   } else {
     // Switch to next player
-    activePlayer = activePlayer === 0 ? 1 : 0;
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
     currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0EL.classList.toggle('player--active');
+    player1EL.classList.toggle('player--active');
   }
 });
